54b37b73fe73e520473c3d48d77d36c82e162a02
Jeffrey Jose <49552078+Jeffreyjose29@users.noreply.github.com>
2019-08-28 20:05:03 +1200

    Finished Q2
    Need to double check with Bob if the combinations of rate and volume for the log logistic model is meant to return negative values for all possible results.

--- Code/STATS321 AssignmentTwo RCode.R
+++ Code/STATS321 AssignmentTwo RCode.R
@@ -5,6 +5,8 @@
 ################################################## QUESTION 1 ################################################################
 
 ##Initial set up for the dataset
+install.packages("ggplot")
+library(ggplot2)
 install.packages("faraway")
 library(faraway)
 data(phbirths)
@@ -46,11 +48,19 @@ xtabs(~ black + smoke, data = phbirths)
 plot(phbirths$black, phbirths$grams, main = "Comparison Of Dist. Of Weights Of Babies From Non-Black & Black Mothers",
      xlab = "Is The Mother Black?", ylab = "Weight (g)")
 
+#Probability distribution of the weight of smoker's babies
+# install.packages("sm")
+# library(sm)
+# sm.density.compare(phbirths$grams, phbirths$smoke)
+# legend("topright", levels(phbirths$smoke), fill = 2+(0:nlevels(phbirths$smoke)))
+
+ggplot(phbirths, aes(x = phbirths$grams, fill = phbirths$smoke)) + geom_density(alpha = 0.3) + ggtitle("Babies Weight Distribution Between Smokers And Non-Smokers") + xlab("Weight (g)") + ylab("Probability") + labs(fill = "Smokes?")
+
 #Using a linear model check the influence of smoke and being black on the weight of the baby
 fit.lm = lm(grams ~ black + smoke, data = phbirths)
 summary(fit.lm)
 
-
+phbirths$smoke
 ################################################### QUESTION 2 ###############################################################
 
 # 0 = non-occurance of vasoconstriction
@@ -115,30 +125,70 @@ gof.log.link.logit
 #The null model using the logit link
 binary.logit.glm.NULL = glm(Response ~ 1, data = finney2, family = binomial(link = logit))
 
-#p-value for goodness of fit for the initial logit link model
-pchisq(deviance(binary.logit.glm.NULL)-deviance(binary.logit.glm),
-       df.residual(binary.logit.glm.NULL)-df.residual(binary.logit.glm),
-       lower.tail=FALSE)
-
-#p-value for the goodness of fit for the log of the logit link model
-pchisq(deviance(binary.logit.glm.NULL)-deviance(log.binary.logit.glm),
-       df.residual(binary.logit.glm.NULL)-df.residual(log.binary.logit.glm),
-       lower.tail=FALSE)
+# #p-value for goodness of fit for the initial logit link model
+# pchisq(deviance(binary.logit.glm.NULL)-deviance(binary.logit.glm),
+#        df.residual(binary.logit.glm.NULL)-df.residual(binary.logit.glm),
+#        lower.tail=FALSE)
+# 
+# #p-value for the goodness of fit for the log of the logit link model
+# pchisq(deviance(binary.logit.glm.NULL)-deviance(log.binary.logit.glm),
+#        df.residual(binary.logit.glm.NULL)-df.residual(log.binary.logit.glm),
+#        lower.tail=FALSE)
 
 
 #Part I
 install.packages('roperators')
 require(roperators)
 
-new.values <- data.frame(Volume = -.8407604, Rate = 4)
-predict(binary.logit.glm, newdata = new.values, type = "response")
-
+volArray <- c()
+ratArray <- c()
+#For the initial logit link model
 for (vol in seq(from = 0.0, to = 4, by = 0.25)){
   x <- solve(2.6491, 7.3324 - (3.8822*vol))
-  print(c(vol, x))
+  #print(c(vol, x))
+  if (x >= 0){
+    volArray <- append(volArray, vol, after = length(volArray))
+    ratArray <- append(ratArray, x, after = length(ratArray))
+  }
 }
 
 for (rat in seq(from = 0.0, to = 4, by = 0.25)){
   y <- solve(3.8822, 7.3324 - (2.6491*rat))
-  print(c(rat, y))
+  #print(c(rat, y))
+  if (y >= 0){
+    volArray <- append(volArray, y, after = length(volArray))
+    ratArray <- append(ratArray, rat, after = length(ratArray))
+  }
+}
+
+#For the log logit link model
+for (LogVol in seq(from = 0.0, to = 4, by = 0.25)){
+  x <- solve(4.562, 0.6778 - (5.179*LogVol))
+  print(c(LogVol, x))
+}
+
+for (LogRat in seq(from = 0.0, to = 4, by = 0.25)){
+  y <- solve(5.179, 0.6778 - (4.562*LogRat))
+  print(c(LogRat, y))
+}
+
+#Plotting prediction points on the graph
+
+#split the binary response into a seperate variable called binary and so it is easier to call
+binary  = factor(finney2$Response, c(0,1), labels = c("No Vasoconstriction","Vasoconstriction"))
+#plot the volume vs rate scatterplot defined by the response 
+plot(finney2$Volume, y = finney2$Rate, main = "Relation Between Rate of Inhalation vs. Volume of Air Inhaled", ylab = "Rate (L/s)", 
+     xlab = "Volume (L)", col = c("red","blue")[binary], pch = c(19,4)[binary], cex = 1.5)
+legend(x = "topright", legend = levels(binary), col = c("red", "blue"), pch = c(19,4))
+for(i in seq_along(volArray)){
+  points.default(volArray[i], ratArray[i], type = "p", pch = 21, col = "black", bg = NA, cex = 1.25)
+}
+
+#Plotting points used earlier for prediction
+pred.Vol <- c(1, 3.1, 0.4)
+pred.Rate <- c(0.9, 1.2, 2.6)
+
+for(j in seq_along(pred.Vol)){
+  #print(c(pred.Vol[j], pred.Rate[j]))
+  points.default(pred.Vol[j], pred.Rate[j], type = "p", pch = 23, col = "black", bg = "yellow", cex = 1.25)
 }
